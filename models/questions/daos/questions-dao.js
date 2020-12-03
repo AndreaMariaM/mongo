6023d0a219e3ca81022ba43c692a74ecc963e72c
AndreaMariaM <matellian.a@husky.neu.edu>
2020-12-02 22:12:40 -0500

    dao

---
+++ models/questions/daos/questions-dao.js
@@ -0,0 +1,5 @@
+const questionsModel = require('../models/questions/questions-model.js')
+const findAllQuestions = () => questionsModel.find()
+const findQuestionForQuiz = (qzId) => questionModel.find("quizId"=qzId)
+const findQuestionById = (questionId) => questionModel.findById(questionId)
+module.exports = { findAllQuestions, findQuestionForQuiz findQuestionById }
